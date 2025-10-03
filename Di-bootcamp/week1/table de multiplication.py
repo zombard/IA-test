2731ecf4574c8e7960f15b281369ea9e6ee58537
Francois Neveu (extern) <francois.neveu-extern@ss2qual.com>
2025-10-03 14:56:42 +0200

    création structure

---
+++ Di-bootcamp/week1/table de multiplication.py
@@ -0,0 +1,10 @@
+n=input("nombre ?  ")
+n=int(n)
+it=range(1,11)
+for i in it :
+    print(i,"X",n,"=",i*n)
+print("test   ")
+current_number=1
+while current_number <= 10:    
+    print(current_number,"X",n,"=",i*n)
+    current_number += 1
