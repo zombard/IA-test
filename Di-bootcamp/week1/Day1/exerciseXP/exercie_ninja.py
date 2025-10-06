f68660c29b3f31ad61c6784c35bb90cb48b8848e
Francois Neveu (extern) <francois.neveu-extern@ss2qual.com>
2025-10-06 08:53:28 +0200

    exercice ninja

---
+++ Di-bootcamp/week1/Day1/exerciseXP/exercie_ninja.py
@@ -0,0 +1,44 @@
+print()
+# # Exercise 1 : Outputs
+
+# # Predict the output of the following code snippets:
+
+
+print(3 <= 3 < 9)
+# true
+
+print(3 == 3 == 3)
+# true
+print(bool(0))
+# false
+print(bool(5 == "5"))
+# false
+print(bool(4 == 4) == bool("4" == "4"))
+# true
+print(bool(bool(None)))
+# false
+x = (1 == True)
+print(x)
+# true
+y = (1 == False)
+print(y)
+# False
+
+a = True + 4
+print(a)
+# 5
+b = False + 10
+print(b)
+# 10
+
+print("x is", x)
+# x is true
+print("y is", y)
+# y is false
+print("a:", a)
+#a:5
+print("b:", b)
+b:11
+
+
+ 
